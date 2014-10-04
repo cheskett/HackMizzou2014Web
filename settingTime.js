ca95bc9b5ef134f4e15f251aea666aa9e0f85a31
Alex Wilhelm <alex.wilhelm@oasisdigital.com>
2014-10-04 09:59:06 -0500

    Added TimePicker

---
+++ settingTime.js
@@ -0,0 +1,30 @@
+
+var myDataRef = new Firebase('https://tt1gfbgrp6u.firebaseio-demo.com/');
+//myDataRef.remove();
+$('#messageInput').keypress(function (e) {
+if (e.keyCode == 13) {
+    var name = $('#name').val();
+    var text = $('#messageInput').val();
+    myDataRef.push({name: name, text: text});
+    $('#messageInput').val('');
+}
+});
+
+myDataRef.on('child_added', function(snapshot) {
+    var message = snapshot.val();
+    displayChatMessage(message.name, message.text);
+});
+
+function displayChatMessage(name, text) {
+    $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+    $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+};
+
+ng.ready( function() {
+    var my_timepicker = new ng.TimePicker({
+        input:'my_timepicker_input'
+    });
+    console.log(ng.get('my_timepicker_input'));
+    console.log(my_timepicker);
+    
+});
