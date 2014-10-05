dabfa057f89e21a921133953463c99d721b1643f
derialstrazus <dklee@purdue.edu>
2014-10-05 08:32:04 -0500

    FInished pebble app

---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,85 @@
+var xhrRequest = function (url, type, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.send();
+};
+
+function locationSuccess(pos) {
+  // Construct URL
+  var url = "https://pill-reminder.firebaseio.com/.json";
+
+  // Send request to OpenWeatherMap
+  xhrRequest(url, 'GET', 
+    function(responseText) {
+      // responseText contains a JSON object with weather info
+      var json = JSON.parse(responseText);
+
+      var i = 0;
+      
+      while (typeof(json.PETE.drugs[i]) !== undefined){
+        // Temperature in Kelvin requires adjustment
+      var temperature = json.PETE.drugs[i].drugname;
+      console.log("Temperature is " + temperature);
+
+      // Conditions
+      var conditions = json.PETE.drugs[i].time[0];      
+      console.log("Conditions are " + conditions);
+      
+      // Assemble dictionary using our keys
+      var dictionary = {
+        "KEY_TEMPERATURE": temperature,
+        "KEY_CONDITIONS": conditions
+      };
+        Pebble.sendAppMessage(dictionary,
+        function(e) {
+          console.log("Weather info sent to Pebble successfully!");
+        },
+        function(e) {
+          console.log("Error sending weather info to Pebble!");
+        }
+      );
+        
+        
+        i++;
+      }
+      
+      
+
+      // Send to Pebble
+      
+    }      
+  );
+}
+
+function locationError(err) {
+  console.log("Error requesting location!");
+}
+
+function getWeather() {
+  navigator.geolocation.getCurrentPosition(
+    locationSuccess,
+    locationError,
+    {timeout: 15000, maximumAge: 60000}
+  );
+}
+
+// Listen for when the watchface is opened
+Pebble.addEventListener('ready', 
+  function(e) {
+    console.log("PebbleKit JS ready!");
+
+    // Get the initial weather
+    getWeather();
+  }
+);
+
+// Listen for when an AppMessage is received
+Pebble.addEventListener('appmessage',
+  function(e) {
+    console.log("AppMessage received!");
+    getWeather();
+  }                     
+);
---
+++ src/main.c
@@ -0,0 +1,257 @@
+#include <pebble.h>
+#include <string.h>
+  
+#define KEY_TEMPERATURE 0
+#define KEY_CONDITIONS 1
+  
+struct alarm{
+  char *drugName;
+  int minute;
+  int hour;
+  int on;
+};
+
+struct alarm times[10];  
+
+static Window *s_main_window;
+static TextLayer *s_time_layer;
+static TextLayer *s_bottom_layer;
+static TextLayer *s_top_layer;
+
+static GFont s_time_font;
+static GFont s_weather_font;
+
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
+
+static void update_time() {
+    // Get a tm structure
+  time_t temp = time(NULL); 
+  
+  struct tm *tick_time = localtime(&temp);
+  
+  // Create a long-lived buffer
+  static char buffer[] = "00:00";
+
+  // Write the current hours and minutes into the buffer
+  //if(clock_is_24h_style() == true) {
+    //Use 24h hour format
+    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+  //} else {
+    //Use 12 hour format
+  //  strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+  //}
+  
+  int i = 0;
+  
+  while(times[i].on == 1){
+    
+    if(times[i].minute == tick_time->tm_min && times[i].hour == tick_time->tm_hour){
+        vibes_long_pulse();
+        text_layer_set_text(s_top_layer, times[i].drugName);
+        text_layer_set_text(s_bottom_layer, "TAKE PILLS!");  
+        break;
+    }else{
+    // Display this time on the TextLayer
+    text_layer_set_text(s_time_layer, buffer);
+    text_layer_set_text(s_bottom_layer, "Relax...");
+    text_layer_set_text(s_top_layer, "");
+    }  
+    i++;
+  }
+  
+  if(i == 0){
+    text_layer_set_text(s_time_layer, buffer);
+    text_layer_set_text(s_bottom_layer, "Relax...");
+    text_layer_set_text(s_top_layer, "");
+  }
+  
+}
+
+static void main_window_load(Window *window) {
+  //Create GBitmap, then set to created BitmapLayer
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+  s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+  
+  // Create time TextLayer
+  s_time_layer = text_layer_create(GRect(5, 52, 139, 50));
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text(s_time_layer, "00:00");
+  
+  //Create GFont
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
+
+  //Apply to TextLayer
+  text_layer_set_font(s_time_layer, s_time_font);
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  
+  // Create temperature Layer
+  s_bottom_layer = text_layer_create(GRect(0, 130, 144, 25));
+  text_layer_set_background_color(s_bottom_layer, GColorClear);
+  text_layer_set_text_color(s_bottom_layer, GColorWhite);
+  text_layer_set_text_alignment(s_bottom_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_bottom_layer, "Loading...");
+  s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_20));
+  text_layer_set_font(s_bottom_layer, s_weather_font);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_bottom_layer));
+  
+  s_top_layer = text_layer_create(GRect(0, 20, 144, 25));
+  text_layer_set_background_color(s_top_layer, GColorClear);
+  text_layer_set_text_color(s_top_layer, GColorWhite);
+  text_layer_set_text_alignment(s_top_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_top_layer, "Loading...");
+  s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_20));
+  text_layer_set_font(s_top_layer, s_weather_font);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_top_layer));
+  
+  // Make sure the time is displayed from the start
+  update_time();
+}
+
+static void main_window_unload(Window *window) {
+  //Unload GFont
+  fonts_unload_custom_font(s_time_font);
+  
+  //Destroy GBitmap
+  gbitmap_destroy(s_background_bitmap);
+
+  //Destroy BitmapLayer
+  bitmap_layer_destroy(s_background_layer);
+  
+  // Destroy TextLayer
+  text_layer_destroy(s_time_layer);
+  
+  // Destroy weather elements
+  text_layer_destroy(s_bottom_layer);
+  fonts_unload_custom_font(s_weather_font);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+  
+  // Get weather update every 2 minutes
+  if(tick_time->tm_min % 2 == 0) {
+    // Begin dictionary
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+
+    // Add a key-value pair
+    dict_write_uint8(iter, 0, 0);
+
+    // Send the message!
+    app_message_outbox_send();
+  }
+}
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  // Store incoming information
+  static char temperature_buffer[32];
+  static char conditions_buffer[32];
+  static char weather_layer_buffer[32];
+  
+  // Read first item
+  Tuple *t = dict_read_first(iterator);
+
+  // For all items
+
+  for(int i=0;i<10;i++){
+    //times[i].on = 0;
+  }
+  
+  int i = 0;
+  
+  while(times[i].on == 1){
+    i++;
+  }
+  
+  while(t != NULL) {
+    // Which key was received?
+    switch(t->key) {
+    case KEY_TEMPERATURE:
+      snprintf(temperature_buffer, sizeof(temperature_buffer), "%s", t->value->cstring);
+      times[i].drugName = t->value->cstring;
+      break;
+    case KEY_CONDITIONS:
+      snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", t->value->cstring);
+      APP_LOG(APP_LOG_LEVEL_DEBUG,t->value->cstring);
+      times[i].on = 1;
+      times[i].minute = (int) atoi(t->value->cstring) % 100;
+      times[i].hour = (int) atoi(t->value->cstring) / 100;
+      APP_LOG(APP_LOG_LEVEL_DEBUG,"minute = %d",times[i].minute);
+      APP_LOG(APP_LOG_LEVEL_DEBUG,"hour = %d",times[i].hour);
+      i++;
+      break;
+    default:
+      APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
+      break;
+    }
+
+    // Look for next item
+    t = dict_read_next(iterator);
+  }
+  
+  // Assemble full string and display
+  snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
+  //text_layer_set_text(s_bottom_layer, weather_layer_buffer);
+  text_layer_set_text(s_top_layer, "");
+  
+  update_time();
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+  
+static void init() {
+  // Create main Window element and assign to pointer
+  s_main_window = window_create();
+
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+
+  // Show the Window on the watch, with animated=true
+  window_stack_push(s_main_window, true);
+  
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  
+  // Register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+  
+  // Open AppMessage
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+}
+
+static void deinit() {
+  // Destroy Window
+  window_destroy(s_main_window);
+}
+
+int main(void) {
+  for(int i=0;i<10;i++){
+    times[i].on = 0;
+  }
+  init();
+  app_event_loop();
+  deinit();
+}
